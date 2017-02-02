931f7ca4197ee4e464c4d2ba824f8d773613982a
Maikel Yordan Almarales Leyva <maikel.almarales@gmail.com>
2017-02-02 16:22:04 -0500

    Test MySQL connection

--- aprendiendo/settings.py
+++ aprendiendo/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'aprendiendo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'maikelYordan.mysql.pythonanywhere-services.com',
+        'USER':'maikelYordan',
+        'PASSW': 'Puppy32017',
+        'NAME': 'maikelYordan$blog',
     }
 }
 
